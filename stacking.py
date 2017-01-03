f3725ead6818d3b0f4c3ce810f153d81e2b40504
Mikhail Karasikov <karasikov@users.noreply.github.com>
2017-01-03 19:00:06 +0100

    vstack for huge matrices

--- stacking.py
+++ stacking.py
@@ -5,7 +5,7 @@
 
 
 import numpy as np
-from sklearn.cross_validation import KFold
+from sklearn.model_selection import KFold
 from sklearn.base import ClassifierMixin, BaseEstimator
 import scipy
 
@@ -13,20 +13,17 @@ import scipy
 class Stacking(BaseEstimator, ClassifierMixin):
     """Base class for stacking method of learning"""
 
-    def __init__(self, base_estimators, meta_fitter=None, get_folds=None, n_folds=3, extend_meta=False):
+    def __init__(self, base_estimators, meta_fitter, get_folds=None, n_folds=3, extend_meta=False):
         """Initialize Stacking
 
         Input parameters:
             base_estimators --- list of tuples (fit(X, y), predict(clf, X)) -- base estimators
-            meta_fitter --- meta classifier
+            meta_fitter --- fit(X, y)
             split --- split strategy
         """
         self.base_estimators = base_estimators
         self.meta_fitter = meta_fitter
-        if get_folds:
-            self.get_folds = get_folds
-        else:
-            self.get_folds = lambda y, n_folds: KFold(len(y), n_folds, True)
+        self.get_folds = get_folds if get_folds else lambda y, n_folds: KFold(n_folds, True).split(y)
         self.n_folds = n_folds
         self.extend_meta = extend_meta
 
@@ -42,36 +39,31 @@ class Stacking(BaseEstimator, ClassifierMixin):
         """
         self.base_predictors = []
         X = scipy.sparse.csr_matrix(X)
-        y = np.array(y)
+        if isinstance(y, list):
+            y = np.array(y)
 
-        self.X_meta, self.y_meta = [], []
-        for base_subsample, meta_subsample in self.get_folds(y, self.n_folds):
-            meta_features = [X[meta_subsample]] if self.extend_meta else []
-            for fit, predict in self.base_estimators:
-                base_clf = fit(X[base_subsample], y[base_subsample])
-                meta_features.append(
-                    scipy.sparse.csr_matrix(
-                        predict(base_clf, X[meta_subsample]).reshape(meta_subsample.size, -1)
-                    )
-                )
-            self.X_meta.append(scipy.sparse.hstack(meta_features))
-            self.y_meta.extend(y[meta_subsample])
+        X_meta, meta_indices = [], []
+        for _, meta_subsample in self.get_folds(y, self.n_folds):
+            mask_meta = np.zeros(len(y), dtype=bool)
+            mask_meta[meta_subsample] = True
 
-        self.X_meta = scipy.sparse.vstack(self.X_meta)
+            meta_subsample = np.where(mask_meta)[0]
 
-        self.base_classifiers = [(fit(X, y), predict)
-                                    for (fit, predict) in self.base_estimators]
-        if self.meta_fitter:
-            self._fit_meta(self.meta_fitter)
+            meta_indices.append(meta_subsample)
 
-        return self
+            X_meta.append(scipy.sparse.hstack([
+                scipy.sparse.csc_matrix(predict(fit(X[~mask_meta], y[~mask_meta]),
+                                                X[meta_subsample]).reshape(mask_meta.sum(), -1))
+                for fit, predict in self.base_estimators
+            ]).tocsr())
 
-    def _fit_meta(self, meta_fitter):
-        if not hasattr(self, 'X_meta'):
-            raise Exception("Fit base classifiers first")
-        self.meta_classifier = meta_fitter(self.X_meta, self.y_meta)
-        self.__delattr__('X_meta')
-        self.__delattr__('y_meta')
+        meta_indices = np.hstack(meta_indices)
+        X_meta = scipy.sparse.vstack(X_meta)[np.argsort(meta_indices)]
+
+        self.base_classifiers = [(fit(X, y), predict) for (fit, predict) in self.base_estimators]
+
+        X = csr_vstack(X.tocsc().T, X_meta.tocsc().T).T.tocsr() if self.extend_meta else X_meta
+        self.meta_classifier = self.meta_fitter(X, y)
         return self
 
     def predict(self, X):      
@@ -90,11 +82,25 @@ class Stacking(BaseEstimator, ClassifierMixin):
             raise Exception("Fit meta classifier first")
 
         X = scipy.sparse.csr_matrix(X)
-        estimations_meta = [X] if self.extend_meta else []
+        X_meta = scipy.sparse.hstack([
+            scipy.sparse.csc_matrix(predict(base_clf, X).reshape(X.shape[0], -1))
+            for base_clf, predict in self.base_classifiers
+        ]).tocsr()
+        X = csr_vstack(X.tocsc().T, X_meta.tocsc().T).T.tocsr() if self.extend_meta else X_meta
+        return self.meta_classifier.predict(X)
 
-        for base_clf, predict in self.base_classifiers:
-            estimations_meta.append(
-                scipy.sparse.csr_matrix(predict(base_clf, X).reshape(X.shape[0], -1))
-            )
 
-        return self.meta_classifier.predict(scipy.sparse.hstack(estimations_meta))
+def csr_vstack(a, b):
+    """Takes 2 matrices and appends the second one to the bottom of the first one
+
+    Works for big matrices using int64 indices
+    """
+    assert(type(a) is scipy.sparse.csr_matrix)
+    assert(type(b) is scipy.sparse.csr_matrix)
+    assert(a.shape[1] == b.shape[1])
+    a.data = np.hstack((a.data, b.data))
+    a.indices = np.hstack((a.indices, b.indices))
+    a.indptr = np.hstack((a.indptr, (b.indptr.astype(np.int64) + a.nnz)[1:]))
+    a._shape = (a.shape[0] + b.shape[0], b.shape[1])
+    return a
+
